11e3a2afcb3fda73f70d14b7d315b67f34a70966
kai722kai <99271189+kai722kai@users.noreply.github.com>
2022-02-17 11:04:48 -0500

    Add files via upload

---
+++ P1HW1_OlivenciaKai.py
@@ -0,0 +1,9 @@
+num1 = int(input('Enter first number: '))
+num2 = int(input('Enter second number: '))
+num3 = num1 + num2
+num4 = num1 * num2
+print()
+print(num1)
+print(num2)
+print(num3)
+print(num4)
